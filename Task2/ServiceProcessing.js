a2360b40168ae2d1e486128df17e4928109328cc
Farengeyt <Ivantzyo@gmail.com>
2020-05-13 15:19:49 +0300

    Initial commit

---
+++ Task2/ServiceProcessing.js
@@ -0,0 +1,31 @@
+﻿var params = { 'text': ' Jayesh' };
+
+$.ajax({
+    type: "POST",
+    url: "StudentListWebService.asmx/HelloWorld",
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(params),
+    dataType: "json",
+    success: function (data) {
+        console.log(data);
+    },
+    error: function (request, status, error) {
+        console.log(request);
+    }
+});
+
+
+params = { 'mark': 4 };
+$.ajax({
+    type: "POST",
+    url: "StudentListWebService.asmx/GetStudentsGraterThan",
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(params),
+    dataType: "json",
+    success: function (data) {
+        console.log(data);
+    },
+    error: function (request, status, error) {
+        console.log(request);
+    }
+});  
