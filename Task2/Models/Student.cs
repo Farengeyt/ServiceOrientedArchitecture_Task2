ee1fe0d3bf59be1c6f4cfcc8fc166038351b2b10
Farengeyt <Ivantzyo@gmail.com>
2020-05-22 20:21:49 +0300

    added second host for ClientApp

--- Task2/Models/Student.cs
+++ Task2/Models/Student.cs
@@ -12,7 +12,15 @@ namespace Task2.Models
         public string Name { get; set; }
         public float AvgMark { get; set; }
 
-        public Student() { }
+        public Student() 
+        {
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "*");
+                HttpContext.Current.Response.End();
+            }
+        }
 
         public Student(string name, float avgMark)
         {
